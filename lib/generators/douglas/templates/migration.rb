39d5ce971435a7f7fa431093676e7a7ed24dfa3a
Ilija Eftimov <ileeftimov@gmail.com>
2017-04-06 01:03:37 +0200

    {created,updated}_by fields cannot be NULL

--- lib/generators/douglas/templates/migration.rb
+++ lib/generators/douglas/templates/migration.rb
@@ -1,7 +1,7 @@
 class AddCreatedByAndUpdatedByTo<%= table_name.camelize %> < ActiveRecord::Migration<%= migration_version %>
   def change
-    add_column :<%= table_name %>, :created_by, :integer
-    add_column :<%= table_name %>, :updated_by, :integer
+    add_column :<%= table_name %>, :created_by, :integer, null: false
+    add_column :<%= table_name %>, :updated_by, :integer, null: false
 
     add_index :<%= table_name %>, :created_by
     add_index :<%= table_name %>, :updated_by
