0bf34f39a9492f40176f978518c4894134be87f5
Ilija Eftimov <ileeftimov@gmail.com>
2017-04-06 23:56:31 +0200

    Disable Style/PredicateName cop for Douglas::Model.has_stamps method

--- lib/douglas/model.rb
+++ lib/douglas/model.rb
@@ -2,8 +2,10 @@ require 'douglas/model_config'
 
 module Douglas
   module Model
+    # rubocop:disable Style/PredicateName
     def has_stamps
       Douglas::ModelConfig.new(self).setup
     end
+    # rubocop:enable all
   end
 end
